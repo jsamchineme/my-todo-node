0d499489772b4b60410ab65ce86eb0a4fba0ed8c
John Samuel <jsamchineme@gmail.com>
2018-09-13 12:31:48 +0100

    Add ElephantSql database url to development Environment

--- server/config/config.js
+++ server/config/config.js
@@ -14,11 +14,8 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'todos_production',
-    host: '127.0.0.1',
+    use_env_variable: 'TEST_URL',
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
-    port: 5432
   }
 };
--- server/controllers/todoitems.js
+++ server/controllers/todoitems.js
@@ -8,7 +8,7 @@ module.exports = {
         todoId: req.params.todoId
       })
       .then(todoItem => res.status(201).json({ data: todoItem }))
-      .catch(error => res.status(400).send(error));
+      .catch(error => res.status(400).json({ message: 'failed', error }));
   },
   update(req, res) {
     return TodoItem
@@ -21,7 +21,7 @@ module.exports = {
       .then(todoItem => todoItem
         .update(req.body, { fields: Object.keys(req.body) })
         .then(updatedTodoItem => res.status(200).json({ data: updatedTodoItem }))
-        .catch(() => res.status(400)))
+        .catch(error => res.status(400).send(error)))
       .catch(error => res.status(400).send(error));
   },
   destroy(req, res) {
@@ -34,8 +34,8 @@ module.exports = {
       })
       .then(todoItem => todoItem
         .destroy()
-        .then(() => res.status(204))
-        .catch(() => res.status(400)))
+        .then(() => res.send(204))
+        .catch(error => res.status(400).send(error)))
       .catch(error => res.status(400).send(error));
   }
 };
